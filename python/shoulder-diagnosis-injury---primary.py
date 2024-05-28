8ca97c1f4dfb3d7f88351c5c7557843f2c8171c3
Martin Chapman <contact@martinchapman.co.uk>
2024-05-28 11:45:04 +0100

    Update made by Phenoflow

---
+++ python/shoulder-diagnosis-injury---primary.py
@@ -0,0 +1,25 @@
+# Romi Haas, Ljoudmila Busija, Alexandra Gorelik, Denise A O'Connor, Christopher Pierce, Danielle Mazza, Rochelle Buchbinder, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"125594001","system":"snomedct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('shoulder-diagnosis-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["shoulder-diagnosis-injury---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["shoulder-diagnosis-injury---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["shoulder-diagnosis-injury---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
